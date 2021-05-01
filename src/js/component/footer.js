97594df3c03f758fcedce5624bd05575ea3a782a
ccarcache <ceciliocarcache@gmail.com>
2021-05-01 16:34:52 +0000

    Finalizando code dia 19 Star Wars blog reading list

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
 			Made with <i className="fas fa-laptop-code text-danger" /> by
-			{" Diego "} <i className="far fa-smile-beam text-warning" />
+			{" Carcache "} <i className="far fa-smile-beam text-warning" />
 		</p>
 	</footer>
 );
